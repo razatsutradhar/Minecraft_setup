3f8d44d5a2befb1356c25e5952b0cea46e22d305
razatsutradhar <razat74@gmail.com>
2023-12-19 21:40:42 -0500

    setup for a server on ubuntu

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Update and upgrade the machine
+sudo apt update
+sudo apt upgrade -y
+
+# Install Java SDK and JRE
+sudo apt install default-jdk default-jre -y
+
+sudo apt install screen -y
+
+sudo mkdir ~/minecraft
+cd ~/minecraft
+
+
